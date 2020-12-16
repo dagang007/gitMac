cb27d271fc5fc5ff22d9077f09c962904a0c848d
JohnSwift <johnSwift@gmail.com>
2020-12-16 21:30:26 +0800

    取消打印

--- Git04/ViewController.swift
+++ Git04/ViewController.swift
@@ -10,7 +10,7 @@ class ViewController: UIViewController {
     }
 
     private func setui(){
-        print("set ui")
+        
         let l = UILabel(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
         
         l.text = "hello world"
